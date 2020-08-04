4941190f5f7a0357652da28c9a2e9447ccd81550
numan <nayhan@trabilisim.com>
2020-08-04 10:53:59 +0300

    fx

---
+++ Android 201/ListView/app/build/generated/ap_generated_sources/debug/out/com/hrdijital/listview/BR.java
@@ -0,0 +1,11 @@
+package com.hrdijital.listview;
+
+public class BR {
+  public static final int _all = 0;
+
+  public static final int lang = 1;
+
+  public static final int name = 2;
+
+  public static final int selected = 3;
+}
